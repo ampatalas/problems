646c377120a0e1ce00f46cecda037ad7414efcf1
ampatalas <192193@student.pwr.edu.pl>
2016-02-20 15:52:50 +0100

    Initial commit

---
+++ src/ListSum.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+/**
+ * Problem:
+ * Write three functions that compute the sum of the numbers in a given list using
+ * - a for-loop,
+ * - a while-loop,
+ * - and recursion.
+ */
+public class ListSum {
+
+    public int forLoop(int[] list) {
+        int sum = 0;
+        for (int i : list) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public int whileLoop(int[] list) {
+        int index = 0;
+        int sum = 0;
+        while (index < list.length) {
+            sum += list[index++];
+        }
+        return sum;
+    }
+
+    public int recursive(int[] list) {
+        if (list.length == 0) return 0;
+        else {
+            int[] subArray = Arrays.copyOfRange(list, 0, list.length - 1);
+            return list[list.length - 1] + recursive(subArray);
+        }
+    }
+}
