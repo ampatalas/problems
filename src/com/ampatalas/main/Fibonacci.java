0f054ccf1fead6d89a6eb149a6519860f7b300cb
ampatalas <a.m.patalas@gmail.com>
2016-02-20 17:56:24 +0100

    optimized fibonacci

--- src/com/ampatalas/main/Fibonacci.java
+++ src/com/ampatalas/main/Fibonacci.java
@@ -1,11 +1,23 @@
 package com.ampatalas.main;
 
+import java.math.BigInteger;
+
 public class Fibonacci {
 
-    public static int fibonacci(int number) {
+    public static BigInteger fibonacci(int number) {
         if (number < 1) throw new IllegalArgumentException("Minimal argument is 1");
-        if (number == 1) return 0;
-        if (number == 2) return 1;
-        else return Fibonacci.fibonacci(number - 1) + Fibonacci.fibonacci(number - 2);
+        if (number == 1) return BigInteger.ZERO;
+        if (number == 2) return BigInteger.ONE;
+
+        BigInteger nMinusTwo = BigInteger.ZERO;
+        BigInteger nMinusOne = BigInteger.ONE;
+        BigInteger result = BigInteger.ZERO;
+
+        for (int i = 3; i <= number; i++) {
+            result = nMinusTwo.add(nMinusOne);
+            nMinusTwo = nMinusOne;
+            nMinusOne = result;
+        }
+        return result;
     }
 }
--- src/com/ampatalas/test/FibonacciTest.java
+++ src/com/ampatalas/test/FibonacciTest.java
@@ -4,6 +4,8 @@ import com.ampatalas.main.Fibonacci;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.math.BigInteger;
+
 public class FibonacciTest {
 
     @Test(expected = IllegalArgumentException.class)
@@ -13,22 +15,27 @@ public class FibonacciTest {
 
     @Test
     public void fibonacci1() {
-        Assert.assertEquals(0, Fibonacci.fibonacci(1));
+        Assert.assertEquals(BigInteger.ZERO, Fibonacci.fibonacci(1));
     }
 
     @Test
     public void fibonacci2() {
-        Assert.assertEquals(1, Fibonacci.fibonacci(2));
+        Assert.assertEquals(BigInteger.ONE, Fibonacci.fibonacci(2));
     }
 
     @Test
     public void fibonacci3() {
-        Assert.assertEquals(1, Fibonacci.fibonacci(3));
+        Assert.assertEquals(BigInteger.ONE, Fibonacci.fibonacci(3));
     }
 
     @Test
     public void fibonacci10() {
-        Assert.assertEquals(34, Fibonacci.fibonacci(10));
+        Assert.assertEquals(new BigInteger("34"), Fibonacci.fibonacci(10));
+    }
+
+    @Test
+    public void fibonacci100() {
+        Assert.assertEquals(new BigInteger("218922995834555169026"), Fibonacci.fibonacci(100));
     }
 
 }
